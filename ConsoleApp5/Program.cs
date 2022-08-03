cd5d6414956ff2ba5ad71d9f74e84a49853ad410
Octa <octavio.macieira@aarim.com>
2022-08-03 20:36:36 -0300

    first commit

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dist;
+            double gas, medCons;
+
+            Console.WriteLine("Insira a distância percorrida: ");
+            dist = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o consumo de gasolina em litros: ");
+            gas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            medCons = (double) dist / gas;
+
+            Console.WriteLine(medCons.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+            Console.ReadLine();
+        }
+    }
+}
